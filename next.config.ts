6ec211fc3faac0bf5f9851bbe36fbf805d25b033
Z User <z@container>
2026-04-13 20:15:59 +0000

    dbba94e9-ba60-4234-ae8e-3ce99a6d00a1

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,18 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'loremflickr.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
